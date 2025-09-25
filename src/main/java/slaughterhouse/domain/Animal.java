2331630f847cfef2a37cda8b5e27a8854b5204dc
detrimend <rasmusduus19@gmail.com>
2025-09-25 10:45:27 +0200

    animal

--- src/main/java/slaughterhouse/domain/Animal.java
+++ src/main/java/slaughterhouse/domain/Animal.java
@@ -2,4 +2,23 @@ package slaughterhouse.domain;
 
 public class Animal
 {
+    private static int nextId = 1;
+    private int id;
+    private double weight;
+
+    public Animal(double weight)
+    {
+        this.id = nextId++;
+        this.weight = weight;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public double getWeight()
+    {
+        return weight;
+    }
 }
