982b89e8ae2618af2decf18368369517277ff82f
Rasmus Duus <342727@via.dk>
2025-10-22 11:44:11 +0200

    We commit crimes, not code

--- src/main/java/slaughterhouse/dto/DTOFactory.java
+++ src/main/java/slaughterhouse/dto/DTOFactory.java
@@ -43,6 +43,40 @@ public class DTOFactory
    return GetAllAnimalsForProductResponse.newBuilder().addAllAnimals(list).build();
  }
 
+ public static GetAnimalsRequest createGetAnimalsRequest()
+ {
+   return GetAnimalsRequest.newBuilder().build();
+ }
+
+  public static GetAnimalsResponse createGetAnimalsResponse(Animal[] animals)
+  {
+    ArrayList<DTOAnimal> list = new ArrayList<>();
+    for(Animal a: animals)
+      list.add(DTOAnimal.newBuilder()
+          .setId(a.getId())
+          .setWeight(a.getWeight())
+          .build());
+
+    return GetAnimalsResponse.newBuilder().addAllAnimals(list).build();
+  }
+
+  public static GetProductsRequest createGetProductsRequest()
+  {
+    return GetProductsRequest.newBuilder().build();
+  }
+
+  public static GetProductsResponse createGetProductsResponse(Product[] products)
+  {
+    ArrayList<DTOProduct> list = new ArrayList<>();
+    for(Product p: products)
+      list.add(DTOProduct.newBuilder()
+          .setProductType(p.getType())
+          .setId(p.getId())
+          .build());
+
+    return GetProductsResponse.newBuilder().addAllProducts(list).build();
+  }
+
   public static GetAllProductsForAnimalRequest createGetAllProductsForAnimalRequest(
       int animalId)
   {
--- src/main/java/slaughterhouse/server/SlaughterhouseServiceImpl.java
+++ src/main/java/slaughterhouse/server/SlaughterhouseServiceImpl.java
@@ -1,5 +1,18 @@
 package slaughterhouse.server;
 
+import grpcslaughterhouse.SlaughterhouseServiceGrpc;
+import slaughterhouse.persistence.AnimalDAO;
+import slaughterhouse.persistence.ProductDAO;
+
 public class SlaughterhouseServiceImpl
+    extends SlaughterhouseServiceGrpc.SlaughterhouseServiceImplBase
 {
+  private AnimalDAO animalDao;
+  private ProductDAO productDao;
+
+  public SlaughterhouseServiceImpl(AnimalDAO animalDao, ProductDAO productDao)
+  {
+    this.animalDao = animalDao;
+    this.productDao = productDao;
+  }
 }
