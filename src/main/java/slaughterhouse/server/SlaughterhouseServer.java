adeefaa83a534af8c3062dfe2acaca0227494b51
Rasmus Duus <342727@via.dk>
2025-10-22 11:59:52 +0200

    Serverside done??

--- src/main/java/slaughterhouse/server/SlaughterhouseServer.java
+++ src/main/java/slaughterhouse/server/SlaughterhouseServer.java
@@ -1,5 +1,31 @@
 package slaughterhouse.server;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import slaughterhouse.persistence.AnimalDAO;
+import slaughterhouse.persistence.AnimalDAOImpl;
+import slaughterhouse.persistence.ProductDAO;
+import slaughterhouse.persistence.ProductDAOImpl;
+
 public class SlaughterhouseServer
 {
+  private AnimalDAO animalDAO;
+  private ProductDAO productDAO;
+
+  public static void main(String[] args) throws Exception
+  {
+    new SlaughterhouseServer().run();
+  }
+
+  private void run() throws Exception
+  {
+    AnimalDAO animalDAO = AnimalDAOImpl.getInstance();
+    ProductDAO productDAO = ProductDAOImpl.getInstance();
+
+    Server server = ServerBuilder.forPort(9090)
+        .addService(new SlaughterhouseServiceImpl(animalDAO, productDAO)).build();
+
+    server.start();
+    server.awaitTermination();
+  }
 }
